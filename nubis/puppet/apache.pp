e614a86214abdce7ade1478791df82777670cc88
Daniel Hartnell <danielhartnell@gmail.com>
2016-11-30 13:48:00 -0800

    initial commit

---
+++ nubis/puppet/apache.pp
@@ -0,0 +1,54 @@
+# Define how Apache should be installed and configured
+# We should try to recycle the puppetlabs-apache puppet module in the future:
+# https://github.com/puppetlabs/puppetlabs-apache
+#
+
+include nubis_discovery
+
+nubis::discovery::service {
+  $project_name:
+    tags     => [ 'apache' ],
+    port     => 80,
+    check    => '/usr/bin/curl -If http://localhost:80',
+    interval => '30s',
+}
+
+class {
+    'apache':
+        mpm_module          => 'event',
+        default_mods        => true,
+        default_vhost       => false,
+        default_confd_files => false,
+        service_enable      => true,
+        service_ensure      => true;
+    'apache::mod::status':;
+    'apache::mod::remoteip':
+        proxy_ips => [ '127.0.0.1', '10.0.0.0/8' ];
+    'apache::mod::expires':
+        expires_default => 'access plus 30 minutes';
+}
+
+apache::vhost { 'tlscanary':
+    port              => 80,
+    default_vhost     => true,
+    docroot           => '/var/www/html',
+    docroot_owner     => 'root',
+    docroot_group     => 'root',
+    block             => ['scm'],
+    setenvif          => 'X_FORWARDED_PROTO https HTTPS=on',
+    access_log_format => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
+    custom_fragment   => 'FileETag None',
+    headers           => [
+      "set X-Nubis-Version ${project_version}",
+      "set X-Nubis-Project ${project_name}",
+      "set X-Nubis-Build   ${packer_build_name}",
+    ],
+    rewrites          => [
+      {
+        comment      => 'HTTPS redirect',
+        rewrite_cond => ['%{HTTP:X-Forwarded-Proto} =http'],
+        rewrite_rule => ['. https://%{HTTP:Host}%{REQUEST_URI} [L,R=permanent]'],
+      }
+    ]
+}
+
---
+++ nubis/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "account" {
+  default = "planet"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "environment" {
+  default = "stage"
+}
+
+variable "service_name" {
+  default = "planet"
+}
+
+variable "ami" {
+  default ="ami-53ff5633"
+}
+
+variable "ssh_key_file" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
