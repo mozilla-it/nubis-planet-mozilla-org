4147f469763b93c5c0f9b680b49fc80f9e701027
Daniel Hartnell <danielhartnell@gmail.com>
2016-12-01 15:38:03 -0800

    copied files into admin-scripts directory on host

---
+++ nubis/puppet/files/update-site.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -l
+# Copy repository contents to web root
+#
+
+/opt/admin-scripts/planet.sh
---
+++ nubis/puppet/update-site.pp
@@ -0,0 +1,40 @@
+file { '/opt/admin-scripts':
+  ensure => 'directory',
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+}
+
+file { '/opt/admin-scripts/update-site.sh':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  source  => 'puppet:///nubis/files/update-site.sh',
+  require => File['/opt/admin-scripts'],
+}
+
+file { '/opt/admin/scripts/planet.sh':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  source  => 'puppet:///nubis/files/planet.sh',
+  require => File['/opt/admin-scripts'],
+}
+
+file { "/etc/nubis.d/${project_name}":
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  content => "#!/bin/bash -l
+# Runs once on instance boot, after all infra services are up and running
+
+# Pull latest version
+/opt/admin-scripts/update-site.sh
+
+# Start serving it
+service ${::apache::params::service_name} start
+"
+}
