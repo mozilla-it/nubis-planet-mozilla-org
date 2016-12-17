26479b684de7a207d6f0d37564b5263a55a28533
Daniel Hartnell <danielhartnell@gmail.com>
2016-12-16 17:50:33 -0800

    added required packages for Planet

--- nubis/puppet/python.pp
+++ nubis/puppet/python.pp
@@ -1,19 +1,11 @@
 # Ensure that Python 2.7 is installed
 
-case $::osfamily {
-  'RedHat': {
-    $python_package            = 'python2.7.5-39.el7_2'
-  }
-  'Debian', 'Ubuntu': {
-    $python_package            = 'python2.7'
-  }
-  default: {
-    fail("Unknown Python package for OS ${::osfamily}")
-  }
+package {
+  'python':
+    ensure => installed
 }
 
 package {
-  'python':
-    ensure => installed,
-    name   => $python_package,
+  'libxslt':
+    ensure => installed
 }
