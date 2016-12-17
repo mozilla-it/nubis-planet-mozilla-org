53f349c14439b4aaeb175671c5eac58e5d8be958
Daniel Hartnell <danielhartnell@gmail.com>
2016-12-16 17:52:32 -0800

    migrated to a single script rather than two

--- nubis/puppet/update-site.pp
+++ nubis/puppet/update-site.pp
@@ -5,23 +5,6 @@ file { '/opt/admin-scripts':
   mode   => '0755',
 }
 
-file { ['/opt/planet', '/opt/planet/build']:
-  ensure  => 'directory',
-  owner   => root,
-  group   => root,
-  mode    => '0755',
-  require => File['/opt/admin-scripts'],
-}
-
-file { '/opt/admin-scripts/update-site.sh':
-  ensure  => file,
-  owner   => root,
-  group   => root,
-  mode    => '0755',
-  source  => 'puppet:///nubis/files/update-site.sh',
-  require => File['/opt/admin-scripts'],
-}
-
 file { '/opt/admin-scripts/planet.sh':
   ensure  => file,
   owner   => root,
@@ -31,37 +14,6 @@ file { '/opt/admin-scripts/planet.sh':
   require => File['/opt/admin-scripts'],
 }
 
-file { '/data/':
-  ensure  => 'directory',
-  owner   => root,
-  group   => root,
-  mode    => '0755',
-}
-
-file { '/data/static/':
-  ensure  => 'directory',
-  owner   => root,
-  group   => root,
-  mode    => '0755',
-  require => File['/data/'],
-}
-
-file { '/data/static/src/':
-  ensure  => 'directory',
-  owner   => root,
-  group   => root,
-  mode    => '0755',
-  require => File['/data/static/'],
-}
-
-file { '/data/static/src/planet.mozilla.org/':
-  ensure  => 'directory',
-  owner   => root,
-  group   => root,
-  mode    => '0755',
-  require => File['/data/static/src/'],
-}
-
 file { "/etc/nubis.d/${project_name}":
   ensure  => file,
   owner   => root,
@@ -69,9 +21,6 @@ file { "/etc/nubis.d/${project_name}":
   mode    => '0755',
   content => "#!/bin/bash -l
 # Runs once on instance boot, after all infra services are up and running
-
-# Pull latest version
-/opt/admin-scripts/update-site.sh
 
 # Start serving it
 service ${::apache::params::service_name} start
