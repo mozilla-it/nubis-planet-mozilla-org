b37119bab2b96c927ecfe4fdcacb07a82d0ad20c
Daniel Hartnell <danielhartnell@gmail.com>
2016-12-23 13:02:07 -0800

    consolidated functionality into planet.sh

--- nubis/puppet/files/update-site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-# Copy repository contents to web root
-#
-
-cd /opt/planet/build/
-git clone https://github.com/mozilla/planet-content.git
-git clone https://github.com/mozilla/planet-source.git
-/opt/admin-scripts/planet.sh
