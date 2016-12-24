e21f9dc5ae71209790c401cc25ba57b91eb93d73
Daniel Hartnell <danielhartnell@gmail.com>
2016-12-23 16:05:50 -0800

    cleaned up symlink additions / removals

--- nubis/puppet/files/planet.sh
+++ nubis/puppet/files/planet.sh
@@ -28,6 +28,10 @@ echo $$ > $LOCKFILE
 
 cd /data/static/build
 
+# "git clone" may fail if the directory is not empty
+# Removing symlinks that may exist
+/opt/admin-scripts/symlink_remove.sh
+
 if [ ! -d "/data/static/build/planet-source/.git" ]; then
   git clone https://github.com/mozilla/planet-source.git ./planet-source
 fi
@@ -36,68 +40,16 @@ if [ ! -d "/data/static/build/planet-content/.git" ]; then
   git clone https://github.com/mozilla/planet-content.git ./planet-content
 fi
 
-# This is embarassingly messy.
-# Testing to ensure everything works. I will rewrite this after that.
-
-function add_symlinks {
-  sudo ln -s ../../../planet-content/branches/bugzilla/theme/ /data/static/build/planet-source/trunk/themes/moz_bugzilla
-  sudo ln -s ../../../planet-content/branches/planet/theme/ /data/static/build/planet-source/trunk/themes/moz_planet
-  sudo ln -s ../../../planet-content/branches/ateam/theme/ /data/static/build/planet-source/trunk/themes/moz_ateam
-  sudo ln -s ../../../planet-content/branches/education/theme/ /data/static/build/planet-source/trunk/themes/moz_education
-  sudo ln -s ../../../planet-content/branches/firefox/theme/ /data/static/build/planet-source/trunk/themes/moz_firefox
-  sudo ln -s ../../../planet-content/branches/firefoxmobile/theme/ /data/static/build/planet-source/trunk/themes/moz_firefoxmobile
-  sudo ln -s ../../../planet-content/branches/firefox-os/theme/ /data/static/build/planet-source/trunk/themes/moz_firefox-os
-  sudo ln -s ../../../planet-content/branches/firefox-ux/theme/ /data/static/build/planet-source/trunk/themes/moz_firefox_ux
-  sudo ln -s ../../../planet-content/branches/interns/theme/ /data/static/build/planet-source/trunk/themes/moz_interns
-  sudo ln -s ../../../planet-content/branches/l10n/theme/ /data/static/build/planet-source/trunk/themes/moz_l10n
-  sudo ln -s ../../../planet-content/branches/mozillaonline/theme/ /data/static/build/planet-source/trunk/themes/moz_mozillaonline
-  sudo ln -s ../../../planet-content/branches/mozillaopennews/theme/ /data/static/build/planet-source/trunk/themes/moz_mozillaopennews
-  sudo ln -s ../../../planet-content/branches/mozreview/theme/ /data/static/build/planet-source/trunk/themes/moz_mozreview
-  sudo ln -s ../../../planet-content/branches/participation/theme/ /data/static/build/planet-source/trunk/themes/moz_participation
-  sudo ln -s ../../../planet-content/branches/planet-de/theme/ /data/static/build/planet-source/trunk/themes/moz_planet_de
-  sudo ln -s ../../../planet-content/branches/projects/theme/ /data/static/build/planet-source/trunk/themes/moz_projects
-  sudo ln -s ../../../planet-content/branches/releng/theme/ /data/static/build/planet-source/trunk/themes/moz_releng
-  sudo ln -s ../../../planet-content/branches/research/theme/ /data/static/build/planet-source/trunk/themes/moz_research
-  sudo ln -s ../../../planet-content/branches/taskcluster/theme/ /data/static/build/planet-source/trunk/themes/moz_taskcluster
-  sudo ln -s ../../../planet-content/branches/thunderbird/theme/ /data/static/build/planet-source/trunk/themes/moz_thunderbird
-  sudo ln -s ../../../planet-content/branches/universalsubtitles/theme/ /data/static/build/planet-source/trunk/themes/moz_univeralsubtitles
-  sudo ln -s ../../../planet-content/branches/webmademovies/theme/ /data/static/build/planet-source/trunk/themes/moz_webmademovies
-  sudo ln -s ../../../planet-content/branches/webmaker/theme/ /data/static/build/planet-source/trunk/themes/moz_webmaker
-}
-
-function remove_symlinks {
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_bugzilla
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_planet
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_ateam
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_education
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_firefox
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_firefoxmobile
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_firefox-os
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_firefox_ux
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_interns
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_l10n
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_mozillaonline
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_mozillaopennews
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_mozreview
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_participation
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_planet_de
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_projects
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_releng
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_research
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_taskcluster
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_thunderbird
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_univeralsubtitles
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_webmademovies
-  sudo rm -rf /data/static/build/planet-source/trunk/themes/moz_webmaker
-}
-
 # Update repositories
-remove_symlinks
 cd /data/static/build/planet-source && git pull
 rm /data/static/build/planet-content/trunk
 cd /data/static/build/planet-content && git pull
+
+# Add symlink to satisfy planet.py expectations
 ln -s ../planet-source/trunk/ /data/static/build/planet-content/trunk
-add_symlinks
+
+# Restore symlinks
+/opt/admin-scripts/symlink_add.sh
 
 # Generate content
 cd /data/static/build/planet-content
---
+++ nubis/puppet/files/symlink_remove.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# The Planet repositories on GitHub will probably
+# need a pull request in the future to address
+# this. For now, I'm manually adding symlinks to
+# allow planet.sh to successfully build this site
+
+rm -rf /data/static/build/planet-source/trunk/themes/moz_bugzilla
+rm -rf /data/static/build/planet-source/trunk/themes/moz_planet
+rm -rf /data/static/build/planet-source/trunk/themes/moz_ateam
+rm -rf /data/static/build/planet-source/trunk/themes/moz_education
+rm -rf /data/static/build/planet-source/trunk/themes/moz_firefox
+rm -rf /data/static/build/planet-source/trunk/themes/moz_firefoxmobile
+rm -rf /data/static/build/planet-source/trunk/themes/moz_firefox-os
+rm -rf /data/static/build/planet-source/trunk/themes/moz_firefox_ux
+rm -rf /data/static/build/planet-source/trunk/themes/moz_interns
+rm -rf /data/static/build/planet-source/trunk/themes/moz_l10n
+rm -rf /data/static/build/planet-source/trunk/themes/moz_mozillaonline
+rm -rf /data/static/build/planet-source/trunk/themes/moz_mozillaopennews
+rm -rf /data/static/build/planet-source/trunk/themes/moz_mozreview
+rm -rf /data/static/build/planet-source/trunk/themes/moz_participation
+rm -rf /data/static/build/planet-source/trunk/themes/moz_planet_de
+rm -rf /data/static/build/planet-source/trunk/themes/moz_projects
+rm -rf /data/static/build/planet-source/trunk/themes/moz_releng
+rm -rf /data/static/build/planet-source/trunk/themes/moz_research
+rm -rf /data/static/build/planet-source/trunk/themes/moz_taskcluster
+rm -rf /data/static/build/planet-source/trunk/themes/moz_thunderbird
+rm -rf /data/static/build/planet-source/trunk/themes/moz_univeralsubtitles
+rm -rf /data/static/build/planet-source/trunk/themes/moz_webmademovies
+rm -rf /data/static/build/planet-source/trunk/themes/moz_webmaker
