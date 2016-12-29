3c7513630182e4a45929c3a4972e7ed6b966599f
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-12-29 13:42:42 -0500

    Cleanup planet.sh and parallelize a bit

--- nubis/puppet/files/planet.sh
+++ nubis/puppet/files/planet.sh
@@ -4,33 +4,50 @@
 # shellcheck disable=SC2015
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -E0 -en "$0" "$0" "$@" && exit 0
 
-PLANETS="planet education mozillaonline bugzilla firefox firefoxmobile webmaker
-firefox-ux webmademovies planet-de universalsubtitles interns research
-mozillaopennews l10n ateam projects thunderbird firefox-os releng participation
-taskcluster mozreview planet"
+PLANETS="\
+planet \
+education \
+mozillaonline \
+bugzilla \
+firefox \
+firefoxmobile \
+webmaker
+firefox-ux \
+webmademovies \
+planet-de \
+universalsubtitles \
+interns \
+research
+mozillaopennews \
+l10n \
+ateam \
+projects \
+thunderbird \
+firefox-os \
+releng \
+participation \
+taskcluster \
+mozreview \
+planet \
+"
 
 cd /data/static/build || exit 1
-
-#XXX: Debug
-
-echo "Running with HTTP proxy: $HTTP_PROXY"
-echo "Running with HTTPS proxy: $HTTPS_PROXY"
 
 # "git clone" may fail if the directory is not empty
 # Removing symlinks that may exist
 /opt/admin-scripts/symlink_remove.sh
 
 if [ ! -d "/data/static/build/planet-source/.git" ]; then
-  git clone https://github.com/mozilla/planet-source.git ./planet-source
+  git clone https://github.com/mozilla/planet-source.git planet-source
 fi
 
 if [ ! -d "/data/static/build/planet-content/.git" ]; then
-  git clone https://github.com/mozilla/planet-content.git ./planet-content
+  git clone https://github.com/mozilla/planet-content.git planet-content
 fi
 
 # Update repositories
 cd /data/static/build/planet-source && git pull
-rm /data/static/build/planet-content/trunk
+rm -f /data/static/build/planet-content/trunk
 cd /data/static/build/planet-content && git pull
 cd /data/static/build || exit 1
 
@@ -41,15 +58,20 @@ ln -s /data/static/build/planet-source/trunk/ /data/static/build/planet-content/
 /opt/admin-scripts/symlink_add.sh
 
 # Generate content
-cd /data/static/build/planet-content || exit 1
-cd branches || exit 1
+cd /data/static/build/planet-content/branches || exit 1
+
+# XXX: GNU parallel a possible candidate here
 for planet in $PLANETS; do
     (
       cd "$planet" || exit 1
-      python ../../../planet-source/trunk/planet.py config.ini
-    )
+      python ../../../planet-source/trunk/planet.py config.ini 2>&1 | sed -e"s/^/[$planet] /g"
+    ) &
 done
 
+# Wait for all planet jobs to complete...
+wait
+
+#XXX: Atomic Rsync here?
 cp -rf /data/static/src/planet.mozilla.org/* /var/www/html
 
 exit 0
