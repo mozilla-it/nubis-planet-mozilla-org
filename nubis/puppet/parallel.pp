adfa2457513c3845b03bf8a6c13fcd5a44678380
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-10 10:17:20 -0500

    simplify installation of parallel

--- nubis/puppet/parallel.pp
+++ nubis/puppet/parallel.pp
@@ -1,7 +1,7 @@
 # http://git.savannah.gnu.org/cgit/parallel.git/tree/README
 
 exec { 'install-gnu-parallel':
-  command => '( wget -O - pi.dk/3 || curl pi.dk/3/ || fetch -o - http://pi.dk/3 ) | bash',
+  command => '/usr/bin/curl pi.dk/3/ | bash',
   creates => '/usr/local/bin/parallel',
   path    => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
 }->
