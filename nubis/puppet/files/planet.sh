1ffa087608c45110138050c5197299ad90f7a4f6
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-10 10:32:30 -0500

    discover planets from source-control directly

--- nubis/puppet/files/planet.sh
+++ nubis/puppet/files/planet.sh
@@ -5,32 +5,6 @@
 exec 200<"$0"
 flock -n 200 || exit 0
 
-PLANETS="\
-ateam \
-bugzilla \
-education \
-firefox \
-firefoxmobile \
-firefox-os \
-firefox-ux \
-interns \
-l10n \
-mozillaonline \
-mozillaopennews \
-mozreview \
-participation \
-planet \
-planet-de \
-projects \
-releng \
-research \
-taskcluster \
-thunderbird \
-universalsubtitles \
-webmademovies \
-webmaker \
-"
-
 cd /data/static/build || exit 1
 
 # "git clone" may fail if the directory is not empty
@@ -64,7 +38,7 @@ cd /data/static/build/planet-content/branches || exit 1
 JOBS=$(( 1 + $(/usr/local/bin/parallel --number-of-cores) * 2 ))
 
 # Run planet in parallel
-echo "$PLANETS" | /usr/local/bin/parallel --trim rl -j "$JOBS" -d " " "cd /data/static/build/planet-content/branches/{} && python ../../../planet-source/trunk/planet.py config.ini 2>&1 | tee /var/log/planet-{}.log | sed -e's/^/[{%}][{}] /g'"
+ls -1 | /usr/local/bin/parallel -j "$JOBS" "cd /data/static/build/planet-content/branches/{} && python ../../../planet-source/trunk/planet.py config.ini 2>&1 | tee /var/log/planet-{}.log | sed -e's/^/[{%}][{}] /g'"
 
 #XXX: Atomic Rsync here?
 cp -rf /data/static/src/planet.mozilla.org/* /var/www/html
