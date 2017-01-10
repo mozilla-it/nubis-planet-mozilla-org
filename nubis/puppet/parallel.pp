8b0a8f2c22b8c616276b7915d7085063f81536a1
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-10 10:12:20 -0500

    tyop galore

--- nubis/puppet/parallel.pp
+++ nubis/puppet/parallel.pp
@@ -5,7 +5,7 @@ exec { 'install-gnu-parallel':
   creates => '/usr/local/bin/parallel',
   path    => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
 }->
-exec {'accept citation request':
+exec {'accept-gnu-parallel-license':
   path    => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
   command => 'echo "will cite" | /usr/local/bin/parallel --citation',
 }
