cb0d8412f7b04d8f552a7d3cc6d5afd250265ff6
Daniel Hartnell <danielhartnell@gmail.com>
2017-02-23 14:15:00 -0800

    removed apt package cache update and added libssl-dev package

--- nubis/puppet/python-deps.pp
+++ nubis/puppet/python-deps.pp
@@ -1,13 +1,8 @@
 # Package requirements to help resolve issue-4 in GitHub
 
-# Ensure everything is updated
-exec { 'apt-get-update':
-  command     => '/usr/bin/apt-get update',
-  refreshonly => true,
-}
-
 # Apt dependencies
 $packages = [
+  'libssl-dev',
   'python-pip',
   'build-essential',
   'libffi-dev',
@@ -17,16 +12,13 @@ $packages = [
 package {
   $packages:
     ensure  => 'installed',
-    require => Exec['apt-get-update'],
 }
 
 # Pip dependencies
 exec { 'pip-idna':
   command => '/usr/bin/pip install idna',
-  require => Exec['apt-get-update'],
 }
 
 exec { 'pip-pyopenssl':
   command => '/usr/bin/pip install pyopenssl',
-  require => Exec['apt-get-update'],
 }
