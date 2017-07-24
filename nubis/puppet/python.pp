3bf553a2e890d1d00017ba497598b1576736b4be
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-07-24 13:57:15 -0400

    use python puppet module

--- nubis/puppet/python.pp
+++ nubis/puppet/python.pp
@@ -1,20 +1,5 @@
-# Ensure that required packages are available
-
-case $::osfamily {
-  'RedHat': {
-    package {
-      'python':
-        ensure => '2.7.5-48.el7',
-        name   => 'python'
-    }
-  }
-  'Debian', 'Ubuntu': {
-    package {
-      'python':
-        name   => 'python2.7'
-    }
-  }
-  default: {
-    fail("Unknown package version for OS ${::osfamily}")
-  }
+class { 'python':
+  version => 'system',
+  pip     => true,
+  dev     => true,
 }
